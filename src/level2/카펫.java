8978ad451e47ca4c3ff0c39d74856e9562585f40
seojw0730 <seojw0730@naver.com>
2024-03-29 08:52:20 +0900

    카펫 완료

---
+++ src/level2/카펫.java
@@ -0,0 +1,25 @@
+package level2;
+
+public class 카펫{
+	public static void main(String[] args) {
+		new Solution().solution(24, 24);
+	}
+}
+
+class Solution{
+	public int[] solution(int brown, int yellow) {
+		int[] answer = new int[2];
+		
+		// 노란색 타일이 i줄일 경우
+		for(int i = 1; i < yellow + 1; i++) {
+			// 갈색 타일은 노란색 타일을 한 겹으로 둘러 쌓음
+			if(brown == (2 * ((yellow / i + yellow % i) + i) + 4)) {
+				answer[0] = (yellow / i) + 2;
+				answer[1] = i + 2;
+				break;
+			}
+		}
+		return answer;
+	}
+	
+}
