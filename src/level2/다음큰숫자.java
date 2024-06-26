0d5dfe6467ab24b4f255acdc375c52950d734dd3
seojw0730 <seojw0730@naver.com>
2024-06-27 08:39:28 +0900

    다음큰숫자 완료

---
+++ src/level2/다음큰숫자.java
@@ -0,0 +1,39 @@
+package level2;
+
+public class 다음큰숫자 {
+	
+	public static void main(String[] args) {
+		int n = 78;
+		System.out.println(solution(n));
+	}
+	
+	public static int solution(int n) {
+		int answer = 0;
+		
+		String nStr = String.valueOf(Integer.toBinaryString(n));
+		// n의 1개수
+		int num = 0;
+		for(int i = 0; i < nStr.length(); i++) {
+			if(nStr.charAt(i) == '1') {
+				num++;
+			}
+		}
+		int num2;
+		n++;
+		while(true) {
+			nStr = String.valueOf(Integer.toBinaryString(n));
+			num2 = 0;
+			for(int i = 0; i < nStr.length(); i++) {
+				if(nStr.charAt(i) == '1') {
+					num2++;
+				}
+			}
+			if(num == num2) {
+				break;
+			}
+			n++;
+		}
+		
+		return answer = n;
+	}
+}
