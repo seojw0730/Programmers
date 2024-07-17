c13f04ed466c199127e70653dbd49c06537f3cc7
서재원 <seojw0730@naver.com>
2024-07-18 00:35:41 +0900

    피보나치수 main solution 구분

--- src/level2/피보나치수.java
+++ src/level2/피보나치수.java
@@ -1,28 +1,30 @@
 package level2;
 
 public class 피보나치수 {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) { 
 		int n = 100;
+		System.out.println(solution(n));
+	}
+	
+	public static int solution(int n) {
 		int answer = 0;
-		
-		int a = 0;
-		int temp1, temp2;
-		temp1 = 1;
-		temp2 = 1;
+		int temp1 = 1;
+		int temp2 = 1;
 		while (true) {
-			a  = temp1 + temp2;
-			if(a > 1234567) {
-				a = a - 1234567;
+			answer  = temp1 + temp2;
+			if(answer > 1234567) {
+				answer -= 1234567;
 			}
 			if ((n - 3) < 1) {
 				break;
 			}
-
+			
 			temp1 = temp2;
-			temp2 = a;
-
+			temp2 = answer;
+			
 			n--;
 		}
-		answer = a;
+		return answer;
 	}
 }
