b56ede35b4ed3872015cd7b53cc0490f1905f0b6
seojw0730 <seojw0730@naver.com>
2024-09-25 17:54:10 +0900

    숫자의표현 완료(검색)

--- src/level2/숫자의표현.java
+++ src/level2/숫자의표현.java
@@ -3,20 +3,27 @@ package level2;
 public class 숫자의표현 {
 	public static void main(String[] args) {
 		int n = 15;
+		System.out.println(solution(n));
+	}
+	
+	static int solution(int n) {
 		int answer = 0;
-		int count = 0;
-		for (int i = 1; i <= n; i++) {
-			for (int end = i ; end <= n; end++) {
-				answer = 0;
-				for (int k = i; k <= end; k++) {
-					answer += k;
-				}
-				if (answer == n) {
-					count++;
-				}
-			}
-		}
-		answer = count;
-		System.out.println("answer:" + answer);
+        
+        for(int i = 1; i <= n; i++){
+            int sum = 0;
+            for(int j = i; j <= n; j++){
+                sum += j;
+                if(sum == n){
+                    answer++;
+                    break;
+                }
+                if(sum > n){
+                    break;
+                }
+            }
+        }
+        
+        
+        return answer;
 	}
 }
