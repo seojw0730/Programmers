8f014f3040ef4825b39ca7802448288db19afb40
seojw0730 <seojw0730@naver.com>
2024-10-05 09:48:37 +0900

    멀리뛰기 완료(검색)

---
+++ src/level2/멀리뛰기.java
@@ -0,0 +1,18 @@
+package level2;
+
+public class 멀리뛰기 {
+	public static void main(String[] args) {
+		int n = 1;
+		System.out.println(solution(n));
+	}
+	
+	public static long solution(int n) {
+		long[] longArr = new long[2001];
+		longArr[1] = 1;
+		longArr[2] = 2;
+		for(int i = 3; i <= n; i++) {
+			longArr[i] = (longArr[i-1] + longArr[i-2]) % 1234567;
+		}
+		return longArr[n];
+	}
+}
