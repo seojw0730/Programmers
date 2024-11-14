76811e6551e77df089421f937af46dc1a9c7cfaf
seojw0730 <seojw0730@naver.com>
2024-11-14 17:16:56 +0900

    행렬의곱셈 완료

---
+++ src/level2/행렬의곱셈.java
@@ -0,0 +1,19 @@
+package level2;
+
+import java.util.*;
+
+public class 행렬의곱셈 {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer=new int[arr1.length][arr2[0].length];
+        for(int i=0; i<answer.length; i++){
+            for(int j=0; j<answer[i].length; j++){
+                int temp=0;
+                for(int k=0; k<arr2.length; k++){
+                    temp+=arr1[i][k]*arr2[k][j];
+                }
+                answer[i][j]=temp;
+            }
+        }
+        return answer;
+    }
+}
