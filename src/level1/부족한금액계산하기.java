35d91ce0e4298d851db1c0c77e5ce05e3ea02959
seojw0730 <seojw0730@naver.com>
2024-12-24 17:26:00 +0900

    부족한 금액 계산하기 완료

---
+++ src/level1/부족한금액계산하기.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class 부족한금액계산하기 {
+	public long solution(int price, int money, int count) {
+        long answer = 0;
+        for(int i=1; i<=count; i++){
+            answer+=price*i;
+        }
+        answer-=money;
+        if(answer<0) answer=0;
+        return answer;
+    }
+}
